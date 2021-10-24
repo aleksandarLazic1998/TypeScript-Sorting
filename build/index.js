5b3c5a69eb749e82da5940e3a5755698cd071201
Aleksandar Lazic <aleksandar.lazic.p@gmail.com>
2021-10-24 13:54:04 +0200

    Refactor Code Structure, Add Feature for sorting characters

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var NumbersArray_1 = require("./NumbersArray");
-var Sort_1 = require("./Sort");
+var NumbersArray_1 = require("./Class/NumbersArray");
+var Sort_1 = require("./Class/Sort");
 var numbersArray = new NumbersArray_1.NumbersArray([10, 14, 0, 7, -2, 5, 9, -5]);
 var sort = new Sort_1.Sort(numbersArray);
 sort.sort();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { NumbersArray } from "./NumbersArray";
-import { Sort } from "./Sort";
+import { NumbersArray } from "./Class/NumbersArray";
+import { Sort } from "./Class/Sort";
 const numbersArray = new NumbersArray([10, 14, 0, 7, -2, 5, 9, -5]);
 const sort = new Sort(numbersArray);
 sort.sort();
